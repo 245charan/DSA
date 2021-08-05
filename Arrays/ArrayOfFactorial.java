ae0ceed4f1da8e217c36ef6d3e5b02a99d52cdff
Sai Charan <47969409+245charan@users.noreply.github.com>
2021-08-06 00:01:32 +0530

    Update ArrayOfFactorial.java

--- Arrays/ArrayOfFactorial.java
+++ Arrays/ArrayOfFactorial.java
@@ -14,3 +14,13 @@ class Solution {
         return ans;
     }
 }
+
+/**
+
+
+Factorial of a non-negative integer, is the multiplication of all integers smaller than or equal to n. 
+For example factorial of 6 is 6*5*4*3*2*1 which is 720.
+
+factorial of i = factorial of i-1 * i
+
+*/
