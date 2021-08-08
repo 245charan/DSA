dc93bf389b41f6678d90cc196e46d52d0e3ec26c
Sai Charan <47969409+245charan@users.noreply.github.com>
2021-08-09 00:45:28 +0530

    Update MergeSortedArrays.java

--- Arrays/MergeSortedArrays.java
+++ Arrays/MergeSortedArrays.java
@@ -1,10 +1,12 @@
-class Solution {
+// Efficent Method 1
+    
+ class Solution {
     public int nextGap(int gap) {
         if (gap <= 1) return 0;
-        return (gap / 2) + (gap % 2);
+        return (gap / 2) + (gap % 2); // ceil the value; ceil(7/2) = 4
     }
     public void merge(int arr1[], int arr2[], int n, int m) {
-        // code here
+        // gap is size of both elements
         int i, j, gap = n + m, tmp;
         for (gap = nextGap(gap); gap > 0; gap = nextGap(gap)) {
             for (i = 0; i + gap < n; i++) {
@@ -35,4 +37,30 @@ class Solution {
         }
 
     }
+}
+
+//Efficent Method 2 
+
+/*
+    => Create a variable to store last value of arr1
+    => Traverse the arr1 from last second from right to left
+    => Traverse the arr2 from right to left
+    => Check weather the arr2 elemt is smaller than arr1 element 
+       then update every ohter element with its left adjacent element.
+    => If we find arr2 elements correct position then update the last element with current element of arr2
+*/
+class Solution{
+public:
+    void merge(int arr1[], int arr2[], int n, int m) {
+	     for(int i=m-1;i>=0;i--){
+	        int last = arr1[n-1];
+	        for(int j=n-2;j>=0 && arr2[i]<arr1[j];j--){
+	            arr1[j+1] = arr1[j];
+	            if(j!=n-2 || last > arr2[i]){
+	                arr1[j+1] = arr2[i];
+	                arr2[i] = last;
+	            }
+	        }
+	    }	    
+	}
 }
