fd05b244e0f82d7c1e1636bdc2162352b3b95a18
E. Sai Charan K. Netha <47969409+245charan@users.noreply.github.com>
2021-08-26 00:06:19 +0530

    Update MatrixChainMultiplication.java

--- Dynamic Programming/MatrixChainMultiplication.java
+++ Dynamic Programming/MatrixChainMultiplication.java
@@ -2,5 +2,19 @@ class Solution{
     static int matrixMultiplication(int N, int arr[])
     {
         // code here
+        int dp[][]=new int[N+1][N+1];
+        for(int i = 0;i < N;i++)
+            dp[i][i] = 0;
+        // L is chain length.
+        for(int L = 2;L < N;L++){
+            for(int i = 1;i < N-L+1;i++){
+                int j = i+L-1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k = i;k < j;k++){
+                    dp[i][j] = Math.min(dp[i][j],dp[i][k] + dp[k+1][j] + arr[i-1]*arr[k]*arr[j]);
+                }
+            }
+        }
+        return dp[1][N-1];
     }
 }
