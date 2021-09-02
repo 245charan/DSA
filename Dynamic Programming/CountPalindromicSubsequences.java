7e14997cef74f3f58910ccf3f3246e379330cda4
E. Sai Charan K. Netha <47969409+245charan@users.noreply.github.com>
2021-09-02 23:15:36 +0530

    Create CountPalindromicSubsequences.java

---
+++ Dynamic Programming/CountPalindromicSubsequences.java
@@ -0,0 +1,32 @@
+/*
+Expected Time Complexity: O(N*N)
+Expected Auxiliary Space: O(N*N)
+
+Example 1:
+
+Input: 
+Str = "abcd"
+Output: 
+4
+Explanation:
+palindromic subsequence are : "a" ,"b", "c" ,"d"
+ 
+
+Example 2:
+
+Input: 
+Str = "aab"
+Output: 
+4
+Explanation:
+palindromic subsequence are :"a", "a", "b", "aa"
+ 
+*/
+
+class Solution
+{
+    long countPS(String str)
+    {
+        // Your code here
+    }
+}
