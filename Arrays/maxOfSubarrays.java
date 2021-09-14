1dcb2c450980ec15fb3e682e9fc0a81cfb3950e9
E. Sai Charan K. Netha <47969409+245charan@users.noreply.github.com>
2021-09-14 23:25:11 +0530

    Update maxOfSubarrays.java

--- Arrays/maxOfSubarrays.java
+++ Arrays/maxOfSubarrays.java
@@ -76,3 +76,20 @@ class Solution
         return al;
     }
 }
+/**
+#Algorithm:  
+
+1.Create a deque to store k elements.
+2.Run a loop and insert first k elements in the deque. Before inserting the element, 
+check if the element at the back of the queue is smaller than the current element, 
+if it is so remove the element from the back of the deque, until all elements left 
+in the deque are greater than the current element. Then insert the current element,
+at the back of the deque.Now, run a loop from k to end of the array.
+3.Print the front element of the deque.
+4.Remove the element from the front of the queue if they are out of the current window.
+5.Insert the next element in the deque. Before inserting the element, check if the element
+at the back of the queue is smaller than the current element, if it is so remove the element
+from the back of the deque, until all elements left in the deque are greater than the current element.
+Then insert the current element, at the back of the deque.
+6.Print the maximum element of the last window.
+*/
